844001469d5ef62ff888208f6a9339852478e881
Nikita <iamlilacpenguin@gmail.com>
2017-11-02 11:39:09 +0300

    examples

--- examples.py
+++ examples.py
@@ -1,21 +1,16 @@
 import pandas as pd
 import numpy as np
-from pandas_query import _, F
+from pandas_query import _
 
-df = pd.DataFrame({'ints': np.random.randint(0, 10, 20)})
-df['squares'] = _['ints'] ** 2
-
-t = (
-    df
-    .groupby('squares')
-    .count()
-    .assign(sqrt=_.index.map(np.sqrt).astype(int))
-    .set_index(_.sqrt.map(str) + ' - ' + _.ints.map(str))
-    [_['ints'].isin({1, 2 ,3})]
-)
-print(t)
+df = pd.DataFrame({'ints': np.random.randint(1, 10, 200)})
+df['squares'] = _['ints'] ** 2  # Column assignment, .__setitem__() function
 
 print(
     df
-    .assign(a=_.apply(F('{:02}: {:02}'.format, _['ints'], _['squares']), axis=1))
+    .groupby('squares')
+    .count()
+    .assign(sqrt=_.index.map(np.sqrt).astype(int))  # .assign() function
+    .set_index(_.sqrt.map(str) + ' - ' + _.ints.map(str))  # .set_index() function
+    [_['ints'].between(1, 20)]  # Selecting data, .__getitem__() function
+    (_['sqrt'].map(np.log10) * _['ints'])  # Evaluating expressions, .__call__() function
 )
