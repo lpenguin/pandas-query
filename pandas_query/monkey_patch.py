d5d76be0d98b5a7cc1ba3ba0b543dd8cbe6a2221
Nikita <iamlilacpenguin@gmail.com>
2017-11-14 15:41:24 +0300

    printing patch

--- pandas_query/monkey_patch.py
+++ pandas_query/monkey_patch.py
@@ -8,7 +8,7 @@ _patched = False
 
 
 def patch_method(pandas_object, method_name):
-    print('Patching method: {}.{}'.format(pandas_object, method_name))
+    print('Patching method: {}.{}.{}'.format(pandas_object.__module__, pandas_object.__name__, method_name))
 
     def wrapper(func):
         orig_method = None
