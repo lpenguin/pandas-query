c4e088f8a8163ae78143300d2a03cd33046cfdf3
Nikita <iamlilacpenguin@gmail.com>
2017-11-17 11:36:03 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pandas-query',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/lpenguin/pandas-query',
     packages=['pandas_query'],
     install_requires=['pandas']
